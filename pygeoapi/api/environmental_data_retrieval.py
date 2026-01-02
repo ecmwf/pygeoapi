49b75e04d7ae24ff42f31f8e6987d37b996cab1c
Adam Warde <adamjwarde@gmail.com>
2026-01-02 09:50:19 +0100

    Feature/add corridor (#3)
    * Add corridor to base image
    
    * Set corridor args to none if not used

--- pygeoapi/api/environmental_data_retrieval.py
+++ pygeoapi/api/environmental_data_retrieval.py
@@ -400,6 +400,12 @@ def get_collection_edr_query(
         within = request.params.get("within")
         within_units = request.params.get("within-units")
 
+    corridor_width = corridor_height = None
+    if query_type == "corridor":
+        LOGGER.debug("Processing corridor specific parameters")
+        corridor_width = request.params.get("corridor-width")
+        corridor_height = request.params.get("corridor-height")
+
     LOGGER.debug("Processing z parameter")
     z = request.params.get("z")
 
@@ -449,6 +455,8 @@ def get_collection_edr_query(
         bbox=bbox,
         within=within,
         within_units=within_units,
+        corridor_width=corridor_width,
+        corridor_height=corridor_height,
         limit=limit,
         location_id=location_id,
     )
